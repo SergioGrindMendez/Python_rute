f2320328c8d87b50b10cba9bc9ce2b88b0e8cb32
Sergio Grind <sergiomendezesquivel@gmail.com>
2022-07-06 17:53:07 -0500

    reacomodo_carpetas

--- python_basic/conversor_de_monedas.py
+++
@@ -1,7 +0,0 @@
-pesos = input("cuantos pesos mexicanos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 21
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
