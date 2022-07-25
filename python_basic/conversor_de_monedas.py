111cc0de958a23f45f72eb11bb50b0f558d54729
Sergio Grind <sergiomendezesquivel@gmail.com>
2022-07-25 17:21:01 -0500

    add condicional y mejora de codigo

--- python_basic/conversor_de_monedas.py
+++ python_basic/conversor_de_monedas.py
@@ -1,7 +1,31 @@
-pesos = input("cuantos pesos mexicanos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 21
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("cuantos pinches y devaluados pesos mexicanos tienes?: " + tipo_pesos )
+    pesos = float(pesos)
+    valor_dolar = 21
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 3)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + " pesos" )
+
+menu = """
+Bienvenidos al coversor de MXN a monedas de latam 👻
+
+Elige una
+
+1 - Pesos Colombianos
+2 - Pesos Argentinos
+3 - Pesos Chilenos
+
+opcion: """
+
+opcion = int(input(menu))
+if opcion == 1:
+    conversor( 28000)
+
+elif opcion == 2:
+    conversor( 23305)
+
+elif opcion == 3:
+    conversor( 20)
+else:
+    print("Ingresa una opcion correcta, no mms")
